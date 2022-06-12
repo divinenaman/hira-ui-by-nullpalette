699595ed4bbee4d54fec4d613202a67af79bd010
Naman Agarwal <naman.agarwal2019@vitstudent.ac.in>
2022-06-13 03:48:13 +0530

    refactor

---
+++ src/stories/atoms/button.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Button } from "../../components";
+
+export default {
+  title: "Example/Button",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>
+    {args.children}
+  </Button>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  backgroundColor: "blue",
+  children: "click",
+};
---
+++ src/stories/atoms/link.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Link } from "../../components";
+
+export default {
+  title: "Example/Link",
+  component: Link,
+} as ComponentMeta<typeof Link>;
+
+const Template: ComponentStory<typeof Link> = (args) => (
+  <Link {...args}>{args.children}</Link>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  to: "#",
+  children: "Click Me!",
+  active: false,
+  textColor: "black",
+  onHoverTextColor: "black",
+  onActiveTextColor: "black",
+  backgroundColor: "transparent",
+  onHoverBackgroundColor: "transparent",
+  onActiveBackgroundColor: "transparent",
+  targetNewTab: false,
+};
