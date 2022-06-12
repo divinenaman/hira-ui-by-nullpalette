315d8d3f33dc84ac21e27784d3e3a89aaf19aca4
Naman Agarwal <naman.agarwal2019@vitstudent.ac.in>
2022-06-13 03:48:34 +0530

    test tailwind

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,20 @@
 import { Button } from "./atoms/Button";
 import { Text } from "./atoms/Text";
 import { Link } from "./atoms/Link";
+import { Overlay } from "./atoms/Overlay";
 
 import { Footer, FooterLinkColumn } from "./molecules/Footer";
 import { NavBar } from "./molecules/NavBar";
 
 import { HamburgerIcon } from "./icons/hamburgerIcon";
 
-export { Text, Link, Button, Footer, FooterLinkColumn, NavBar, HamburgerIcon };
+export {
+  Text,
+  Link,
+  Button,
+  Footer,
+  FooterLinkColumn,
+  NavBar,
+  HamburgerIcon,
+  Overlay,
+};
